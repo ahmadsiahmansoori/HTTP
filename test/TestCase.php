e49fc761c700ce580c52c0dd242b4862f1f11dd3
ahmad <ahmad.s96736@gmail.com>
2025-11-10 17:31:17 +0330

    fixed issue httpLog write file data

--- test/TestCase.php
+++ test/TestCase.php
@@ -1,12 +1,14 @@
-<?php 
+<?php
 
-require_once './src/Http.php';
-require_once './src/HttpForm.php';
-require_once './src/HttpResult.php';
-require_once './src/HttpClient.php';
+require_once '../src/Http.php';
+require_once '../src/HttpForm.php';
+require_once '../src/HttpResult.php';
+require_once '../src/HttpClient.php';
+require_once '../src/HttpLog.php';
 
 
-class TestCase {
+class TestCase
+{
 
     const BASE_URL = 'https://jsonplaceholder.typicode.com';
 
@@ -19,21 +21,22 @@ class TestCase {
         "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
        }
      */
-    static function testGet() {
+    static function testGet()
+    {
         $url = self::BASE_URL . '/posts/1';
-        return HttpClient::get($url);
-
+        return HttpClient::get($url, [], [], true);
     }
 
-    static function testPost() {
+    static function testPost()
+    {
         $url = self::BASE_URL . '/posts';
         $payload = ['title' => 'foo', 'body' => 'bar', 'userId' => 1];
-        return HttpClient::post($url, $payload, [], ['Content-Type' => 'application/json'], HttpForm::FORMAT_JSON);
-
+        return HttpClient::post($url, $payload, [], ['Content-Type' => 'application/json'], HttpForm::FORMAT_JSON, ['only_error' => false, 'log_name_file' => 'testCaseMethodPost']);
     }
 
 
-    static function testPatch() {
+    static function testPatch()
+    {
         $url = self::BASE_URL . '/posts/1';
         $payload = ['title' => 'patched title new'];
         return HttpForm::init($url)
@@ -45,7 +48,8 @@ class TestCase {
     }
 
 
-    static function testPut() {
+    static function testPut()
+    {
         $url = self::BASE_URL . '/posts/1';
         $payload = ['id' => 1, 'title' => 'replaced', 'body' => 'content', 'userId' => 1];
         return HttpForm::init($url)
@@ -57,21 +61,29 @@ class TestCase {
     }
 
 
-    static function testDelete() {
+    static function testDelete()
+    {
         $url = self::BASE_URL . '/posts/1';
-    
+
         return HttpForm::init($url)
             ->method(HttpForm::METHOD_DELETE)
             ->curl();
     }
 
 
-    static function testMultiCurl() {
+    static function testMultiCurl()
+    {
 
 
-        $form1 = HttpForm::init(self::BASE_URL . '/posts/1')->method(HttpForm::METHOD_GET);
-        $form2 = HttpForm::init(self::BASE_URL . '/users/1')->method(HttpForm::METHOD_GET);
-        $form3 = HttpForm::init(self::BASE_URL . '/todos/1')->method(HttpForm::METHOD_GET);
+        $form1 = HttpForm::init(self::BASE_URL . '/posts/1')
+            ->activeLog(false, 'test1')
+            ->method(HttpForm::METHOD_GET);
+        $form2 = HttpForm::init(self::BASE_URL . '/users/1')
+            ->activeLog(false, 'test2')
+            ->method(HttpForm::METHOD_GET);
+        $form3 = HttpForm::init(self::BASE_URL . '/todos/1')
+            ->activeLog(false, 'test3')
+            ->method(HttpForm::METHOD_GET);
 
         $url = self::BASE_URL . '/posts';
         $payload = ['title' => 'foo', 'body' => 'bar', 'userId' => 1];
@@ -84,6 +96,8 @@ class TestCase {
             $form4
         );
     }
- 
-    
 }
+
+// var_dump(TestCase::testPost());
+// var_dump(TestCase::testGet());
+var_dump(TestCase::testMultiCurl());
